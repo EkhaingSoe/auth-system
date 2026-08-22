0a4402f19d13c0a31751c23b1f47477c575443b5
Ei Khaing Soe <eikhaingsoe.ycc@gmail.com>
2026-08-22 16:35:57 +0630

    addinng role such as   ROLE_INVENTORY_OFFICER,     ROLE_WAREHOUSE_MANAGER

--- auth-system/src/main/java/com/example/auth_system/permission/enums/RoleName.java
+++ auth-system/src/main/java/com/example/auth_system/permission/enums/RoleName.java
@@ -2,8 +2,13 @@ package com.example.auth_system.permission.enums;
 
 public enum RoleName {
     ROLE_CUSTOMER,
+
     ROLE_MANAGER,
     ROLE_CASHIER,
+
     ROLE_WAREHOUSE_STAFF,
+    ROLE_INVENTORY_OFFICER,
+    ROLE_WAREHOUSE_MANAGER,
+
     ROLE_ADMIN
 }
